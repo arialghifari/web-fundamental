2d1b09be7e9b3e6dd2888e5d7a93bc86f9d2574c
arialghifari <arialghifari99@gmail.com>
2022-08-27 09:45:17 +0800

    ClubFinder: using webpack

---
+++ ClubFinder/src/app.js
@@ -0,0 +1,9 @@
+import "./styles/style.css";
+import "regenerator-runtime";
+import "./script/component/app-bar.js";
+import "./script/component/search-bar.js";
+import "./script/component/club-list.js";
+import "./script/component/club-item.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ ClubFinder/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
